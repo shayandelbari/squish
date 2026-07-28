16304ea832d9e9a227cd140560d250ccc00c1758
shayan <shayandelbari0@gmail.com>
2026-07-28 16:39:21 -0400

    added small mock interactive menu with keyboard navigation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use ratatui::{DefaultTerminal, Frame};
+use crossterm::event::{ Event, KeyCode };
+use ratatui::{
+    DefaultTerminal,
+    Frame,
+    layout::{ Constraint, Layout },
+    style::{ Color, Modifier },
+    widgets::{ Block, Borders, List, ListState },
+};
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
@@ -7,14 +14,33 @@ fn main() -> color_eyre::Result<()> {
 }
 
 fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    let mut list_state = ListState::default().with_selected(Some(0));
     loop {
-        terminal.draw(render)?;
-        if crossterm::event::read()?.is_key_press() {
-            break Ok(());
+        terminal.draw(|frame| render(frame, &mut list_state))?;
+        if let Event::Key(key) = crossterm::event::read()? {
+            match key.code {
+                KeyCode::Char('j') | KeyCode::Down => list_state.select_next(),
+                KeyCode::Char('k') | KeyCode::Up => list_state.select_previous(),
+                KeyCode::Char('q') | KeyCode::Esc => {
+                    break Ok(());
+                }
+                _ => {}
+            }
         }
     }
 }
 
-fn render(frame: &mut Frame) {
-    frame.render_widget("hello world", frame.area());
+fn render(frame: &mut Frame, list_state: &mut ListState) {
+    let layout = Layout::vertical([Constraint::Percentage(100)]).margin(1);
+    let [top] = frame.area().layout(&layout);
+
+    let items = ["Compress", "Decompress", "Quit"];
+
+    let list = List::new(items)
+        .style(Color::White)
+        .highlight_style(Modifier::REVERSED)
+        .highlight_symbol("> ")
+        .block(Block::new().borders(Borders::ALL).title("SQUISH"));
+
+    frame.render_stateful_widget(list, top, list_state);
 }
