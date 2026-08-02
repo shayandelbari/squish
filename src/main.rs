1231482e747e11a060932c4e3e55e65245e88ecd
shayan <shayandelbari0@gmail.com>
2026-08-02 13:10:02 -0400

    added some structure

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use crossterm::event::{ Event, KeyCode };
+use crossterm::event::{Event, KeyCode};
 use ratatui::{
-    DefaultTerminal,
-    Frame,
-    layout::{ Constraint, Layout },
-    style::{ Color, Modifier },
-    widgets::{ Block, Borders, List, ListState },
+    DefaultTerminal, Frame,
+    layout::{Constraint, Layout},
+    style::{Color, Modifier},
+    widgets::{Block, Borders, List, ListItem, ListState},
 };
 
 fn main() -> color_eyre::Result<()> {
@@ -14,15 +13,43 @@ fn main() -> color_eyre::Result<()> {
 }
 
 fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-    let mut list_state = ListState::default().with_selected(Some(0));
+    let menu = Menu {
+        items: vec![
+            MenuItem {
+                label: "Compress".into(),
+                screen: Screen::Compress,
+            },
+            MenuItem {
+                label: "Decompress".into(),
+                screen: Screen::Decompress,
+            },
+            MenuItem {
+                label: "Quit".into(),
+                screen: Screen::Quit,
+            },
+        ],
+        screen: Screen::Home,
+    };
+
+    let mut app = App {
+        current_screen: Screen::Home,
+        list_state: ListState::default().with_selected(Some(0)),
+        menu: &menu,
+    };
+
     loop {
-        terminal.draw(|frame| render(frame, &mut list_state))?;
+        terminal.draw(|frame| render(frame, &mut app))?;
         if let Event::Key(key) = crossterm::event::read()? {
             match key.code {
-                KeyCode::Char('j') | KeyCode::Down => list_state.select_next(),
-                KeyCode::Char('k') | KeyCode::Up => list_state.select_previous(),
+                KeyCode::Char('j') | KeyCode::Down => app.list_state.select_next(),
+                KeyCode::Char('k') | KeyCode::Up => app.list_state.select_previous(),
                 KeyCode::Char('q') | KeyCode::Esc => {
                     break Ok(());
+                }
+                KeyCode::Enter => {
+                    if let Some(index) = app.list_state.selected() {
+                        app.current_screen = menu.items[index].screen;
+                    }
                 }
                 _ => {}
             }
@@ -30,11 +57,15 @@ fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
     }
 }
 
-fn render(frame: &mut Frame, list_state: &mut ListState) {
+fn render(frame: &mut Frame, app: &mut App) {
+    let items = app
+        .menu
+        .items
+        .iter()
+        .map(|item| ListItem::new(item.label.as_str()));
+
     let layout = Layout::vertical([Constraint::Percentage(100)]).margin(1);
     let [top] = frame.area().layout(&layout);
-
-    let items = ["Compress", "Decompress", "Quit"];
 
     let list = List::new(items)
         .style(Color::White)
@@ -42,5 +73,29 @@ fn render(frame: &mut Frame, list_state: &mut ListState) {
         .highlight_symbol("> ")
         .block(Block::new().borders(Borders::ALL).title("SQUISH"));
 
-    frame.render_stateful_widget(list, top, list_state);
+    frame.render_stateful_widget(list, top, &mut app.list_state);
+}
+
+struct MenuItem {
+    label: String,
+    screen: Screen,
+}
+
+struct Menu {
+    items: Vec<MenuItem>,
+    screen: Screen,
+}
+
+struct App<'a> {
+    current_screen: Screen,
+    list_state: ListState,
+    menu: &'a Menu,
+}
+
+#[derive(Clone, Copy)]
+enum Screen {
+    Home,
+    Compress,
+    Decompress,
+    Quit,
 }
