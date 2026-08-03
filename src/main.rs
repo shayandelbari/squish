aba9aed5f8ff56dff62697777f0a282c1ef1bff7
shayan <shayandelbari0@gmail.com>
2026-08-03 17:43:06 -0400

    refined app struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use crossterm::event::{Event, KeyCode};
+use std::collections::HashMap;
+
+use crossterm::event::{ Event, KeyCode };
 use ratatui::{
-    DefaultTerminal, Frame,
-    layout::{Constraint, Layout},
-    style::{Color, Modifier},
-    widgets::{Block, Borders, List, ListItem, ListState},
+    DefaultTerminal,
+    Frame,
+    layout::{ Constraint, Layout },
+    style::{ Color, Modifier },
+    widgets::{ Block, Borders, List, ListItem, ListState },
 };
+
+use crate::Action::{ Open, Quit };
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
@@ -12,43 +17,57 @@ fn main() -> color_eyre::Result<()> {
     Ok(())
 }
 
-fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-    let menu = Menu {
+fn init() -> App {
+    let home_menu = Menu {
         items: vec![
             MenuItem {
                 label: "Compress".into(),
-                screen: Screen::Compress,
+                action: Open(Screen::Compress),
             },
             MenuItem {
                 label: "Decompress".into(),
-                screen: Screen::Decompress,
+                action: Open(Screen::Decompress),
             },
             MenuItem {
                 label: "Quit".into(),
-                screen: Screen::Quit,
-            },
+                action: Quit,
+            }
         ],
-        screen: Screen::Home,
+        list_state: ListState::default().with_selected(Some(0)),
     };
 
     let mut app = App {
         current_screen: Screen::Home,
-        list_state: ListState::default().with_selected(Some(0)),
-        menu: &menu,
+        menus: HashMap::new(),
     };
 
+    app.insert_menu(home_menu, Screen::Home);
+
+    app
+}
+
+fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    let mut app = init();
+
     loop {
-        terminal.draw(|frame| render(frame, &mut app))?;
+        terminal.draw(|frame| render(frame, &mut app.current_menu()))?;
         if let Event::Key(key) = crossterm::event::read()? {
             match key.code {
-                KeyCode::Char('j') | KeyCode::Down => app.list_state.select_next(),
-                KeyCode::Char('k') | KeyCode::Up => app.list_state.select_previous(),
+                KeyCode::Char('j') | KeyCode::Down => app.current_menu().list_state.select_next(),
+                KeyCode::Char('k') | KeyCode::Up => app.current_menu().list_state.select_previous(),
                 KeyCode::Char('q') | KeyCode::Esc => {
                     break Ok(());
                 }
                 KeyCode::Enter => {
-                    if let Some(index) = app.list_state.selected() {
-                        app.current_screen = menu.items[index].screen;
+                    if let Some(index) = app.current_menu().list_state.selected() {
+                        match app.current_menu().items[index].action {
+                            Open(screen) => {
+                                app.current_screen = screen;
+                            }
+                            Quit => {
+                                break Ok(());
+                            }
+                        }
                     }
                 }
                 _ => {}
@@ -57,12 +76,8 @@ fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
     }
 }
 
-fn render(frame: &mut Frame, app: &mut App) {
-    let items = app
-        .menu
-        .items
-        .iter()
-        .map(|item| ListItem::new(item.label.as_str()));
+fn render(frame: &mut Frame, menu: &mut Menu) {
+    let items = menu.items.iter().map(|item| item.label.as_str());
 
     let layout = Layout::vertical([Constraint::Percentage(100)]).margin(1);
     let [top] = frame.area().layout(&layout);
@@ -73,29 +88,42 @@ fn render(frame: &mut Frame, app: &mut App) {
         .highlight_symbol("> ")
         .block(Block::new().borders(Borders::ALL).title("SQUISH"));
 
-    frame.render_stateful_widget(list, top, &mut app.list_state);
+    frame.render_stateful_widget(list, top, &mut menu.list_state);
 }
 
 struct MenuItem {
     label: String,
-    screen: Screen,
+    action: Action,
 }
 
 struct Menu {
     items: Vec<MenuItem>,
-    screen: Screen,
-}
-
-struct App<'a> {
-    current_screen: Screen,
     list_state: ListState,
-    menu: &'a Menu,
 }
 
-#[derive(Clone, Copy)]
+struct App {
+    current_screen: Screen,
+    menus: HashMap<Screen, Menu>,
+}
+
+impl App {
+    fn current_menu(&mut self) -> &mut Menu {
+        return self.menus.get_mut(&self.current_screen).unwrap();
+    }
+
+    fn insert_menu(&mut self, menu: Menu, screen: Screen) {
+        self.menus.insert(screen, menu);
+    }
+}
+
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
 enum Screen {
     Home,
     Compress,
     Decompress,
+}
+
+enum Action {
+    Open(Screen),
     Quit,
 }
